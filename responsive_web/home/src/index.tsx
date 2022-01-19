9f0d83c5d8d9473e644d6f3a819501d871cf03c9
안동규 <dg.an@piolink.com>
2022-01-19 17:12:38 +0900

    R #develop router 변경

--- responsive_web/home/src/index.tsx
+++ responsive_web/home/src/index.tsx
@@ -3,13 +3,13 @@ import './assets/css/login.css';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { HashRouter, Routes } from 'react-router-dom';
 
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
   document.getElementById('root')
 );
