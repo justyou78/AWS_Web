b87faf47d3d289b641e0991ad5341ed488ac952d
안동규 <dg.an@piolink.com>
2022-09-06 12:59:57 +0900

    R #develop Floating action button

---
+++ component/floating_action_menu_button/assets/js/main.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('floating-toggle');
+
+const activeMenu = () => {
+  toggleButton.classList.toggle('active');
+};
+
+toggleButton.addEventListener('click', activeMenu);
